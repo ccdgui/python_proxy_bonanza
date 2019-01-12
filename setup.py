4c7c76b8641b9e7444d8dd9041354063029aa901
Cam <camille.guinemer@gmail.com>
2019-01-12 17:22:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ import os
 from codecs import open
 from os.path import abspath, dirname, join
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    
 from setuptools import setup, find_packages
 
 this_dir = abspath(dirname(__file__))
